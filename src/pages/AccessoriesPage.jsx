38031f0b9c5f54955f28fa1b02b6822ad1662642
7ij0d <7ij0d@github.com>
2026-09-19 21:39:11 +0200

    Add Yellow color 3-view high-res images for 16 inch Dental Box

--- src/pages/AccessoriesPage.jsx
+++ src/pages/AccessoriesPage.jsx
@@ -51,7 +51,7 @@ const FALLBACK_BOXES = [
     inside_image: `${BASE}accessories/box16-inside-maroon.jpg`,
     colors: [
       { id: 'maroon', hex_code: '#800020', label_ar: 'خمري / عنابي', label_en: 'Maroon', image_url: `${BASE}accessories/box16-maroon.jpg`, inside_image_url: `${BASE}accessories/box16-inside-maroon.jpg`, base_image_url: `${BASE}accessories/box16-base-maroon.jpg` },
-      { id: 'yellow', hex_code: '#F5C518', label_ar: 'Yellow', label_en: 'Yellow', image_url: `${BASE}accessories/box16-colors.png` },
+      { id: 'yellow', hex_code: '#F5C518', label_ar: 'أصفر', label_en: 'Yellow', image_url: `${BASE}accessories/box16-yellow.jpg`, inside_image_url: `${BASE}accessories/box16-inside-yellow.jpg`, base_image_url: `${BASE}accessories/box16-base-yellow.jpg` },
       { id: 'red',    hex_code: '#E02020', label_ar: 'Red',    label_en: 'Red',    image_url: `${BASE}accessories/box16-colors.png` },
       { id: 'purple', hex_code: '#7B3FE4', label_ar: 'Purple', label_en: 'Purple', image_url: `${BASE}accessories/box16-colors.png` },
       { id: 'blue',   hex_code: '#1565C0', label_ar: 'Blue',   label_en: 'Blue',   image_url: `${BASE}accessories/box16-colors.png` },
